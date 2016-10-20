4c10cdf3fa27b18e6ad048c5340973983a2cd0cc
Ziva Avdiav <zivavdiav@zivas-air.home>
2016-10-20 17:42:24 +0300

    updated to go through all numbers instead of random numbers

--- recurse_cracklepop_code.py
+++ recurse_cracklepop_code.py
@@ -3,19 +3,19 @@ Program that prints out the numbers 1 to 100 (inclusive).
 If the number is divisible by 3, print Crackle instead of the number. 
 If it's divisible by 5, print Pop. If it's divisible by both 3 and 5, print CracklePop.
 '''
-from random import randint
+
 
 def divisible_number(): 
-divisible_number = randint(0,100)
-
-if (divisible_number / 3 == int):
-  print "Crackle"
-elif (divisible_number / 5 == int):
-  print "Pop"
-elif (divisible_number / 3 == int and divisible_number / 5 == int): 
-  print "CracklePop" 
-else: 
-  print(divisible_number) 
+  
+  for i in range(1,101): 
+    if (i % 3 == 0 and i % 5 == 0):
+      print "CracklePop"
+    elif (i % 3 == 0):
+      print "Crackle"
+    elif (i % 5 == 0):
+      print "Pop"
+    else: 
+      print(i) 
 
 if __name__ == "__main__":
   divisible_number()
